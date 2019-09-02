71d72cd5b7ef4a17500e8e3719182cc99024dfeb
Md. Kamrul Hasan <mhgolap11@gmail.com>
2019-09-02 18:25:56 +0600

    Some dummy features added

--- app/src/main/java/com/mkhglab/kotlintabbar/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/mkhglab/kotlintabbar/fragments/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.mkhglab.kotlintabbar.fragments.profile
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import com.mkhglab.kotlintabbar.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment: Fragment() {
 
@@ -16,6 +20,19 @@ class ProfileFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         mInterface?.profileLoaded()
+
+        view.findViewById<LinearLayout>(R.id.nameDisplayLL).visibility = View.VISIBLE
+        view.findViewById<LinearLayout>(R.id.nameEditLL).visibility = View.GONE
+
+        view.findViewById<Button>(R.id.editButton).setOnClickListener {
+            nameDisplayLL.visibility = View.GONE
+            nameEditLL.visibility = View.VISIBLE
+        }
+
+        view.findViewById<Button>(R.id.submitButton).setOnClickListener {
+            nameDisplayLL.visibility = View.VISIBLE
+            nameEditLL.visibility = View.GONE
+        }
 
         return view
     }
