29fe165fb9d5effbce57f20d962043cbe330aa81
Md. Kamrul Hasan <mhgolap11@gmail.com>
2019-09-02 16:46:04 +0600

    [Menu] group menu added to history fragment

--- app/src/main/java/com/mkhglab/kotlintabbar/fragments/history/HistoryFragment.kt
+++ app/src/main/java/com/mkhglab/kotlintabbar/fragments/history/HistoryFragment.kt
@@ -1,9 +1,7 @@
 package com.mkhglab.kotlintabbar.fragments.history
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.mkhglab.kotlintabbar.R
 
@@ -17,7 +15,14 @@ class HistoryFragment: Fragment() {
 
         mInterface?.historyLoaded()
 
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.menu_history_fragment, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     companion object {
--- app/src/main/java/com/mkhglab/kotlintabbar/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/mkhglab/kotlintabbar/fragments/home/HomeFragment.kt
@@ -1,9 +1,7 @@
 package com.mkhglab.kotlintabbar.fragments.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.mkhglab.kotlintabbar.R
 
