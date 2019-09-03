777c1d347267abefed3b2ec8ac86db03079b48d2
Md. Kamrul Hasan <mhgolap11@gmail.com>
2019-09-03 13:23:08 +0600

    [Settings] ChangePIN, MoreApp, Logout view desinged

--- app/src/main/java/com/mkhglab/kotlintabbar/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/mkhglab/kotlintabbar/fragments/settings/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package com.mkhglab.kotlintabbar.fragments.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mkhglab.kotlintabbar.R
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment: Fragment() {
 
@@ -16,6 +19,16 @@ class SettingsFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         mInterface?.settingsLoaded()
+
+        view.changePINLL.setOnClickListener {
+            Log.d("DEBUG", "View.changePINLL tapped")
+        }
+        view.moreAppsLL.setOnClickListener {
+            Log.d("DEBUG", "View.moreAppsLL tapped")
+        }
+        view.logoutLL.setOnClickListener {
+            Log.d("DEBUG", "View.logoutLL tapped")
+        }
 
         return view
     }
